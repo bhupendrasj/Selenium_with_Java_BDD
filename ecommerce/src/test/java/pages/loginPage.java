7980b4c668b273a4a3a95bff9a01790333760fd6
bhupendrasj <bhupendrajagtap900@gmail.com>
2025-07-15 13:25:21 +0530

    gitignore

--- ecommerce/src/test/java/pages/loginPage.java
+++ ecommerce/src/test/java/pages/loginPage.java
@@ -1,18 +1,29 @@
 package pages;
 
+import org.checkerframework.checker.units.qual.m;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import utils.BasePage;
 
-public class loginPage extends BasePage {
+public abstract class loginPage extends BasePage {
     public loginPage(WebDriver driver) {
         super(driver);
         //TODO Auto-generated constructor stub
     }
     // Locators for the login page elements
+    static By myAccountDrp = By.xpath("(//*[contains(text(),'My account')])[2]");
+    static By registerBtn = By.xpath("//*[contains(@id,'column-right')]//*[contains(text(),'Register')]");
     static By usernameField = By.id("username");
     static By passwordField = By.id("password");
     static By loginButton = By.id("login");
+
+    public static void RegisterUser() {
+        waitForElementToBeVisible(myAccountDrp, 10);
+        clickElement(myAccountDrp);
+        waitForElementToBeVisible(registerBtn, 10);
+        clickElement(registerBtn);
+
+    }
 
     public static void enterUsername(String username) {
         driver.findElement(usernameField).sendKeys(username);
--- ecommerce/src/test/java/stepdefinitions/Login.java
+++ ecommerce/src/test/java/stepdefinitions/Login.java
@@ -6,15 +6,20 @@ import io.cucumber.java.en.When;
 import pages.loginPage;
 
 import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
 
-public class Login {
+public class Login extends loginPage {
     // WebDriver driver = Hooks.driver;  // ✅ Getting driver from Hooks
 
-//    loginPage loginPage;
+   public Login(WebDriver driver) {
+        super(driver);
+        //TODO Auto-generated constructor stub
+    }
 
 @Given("user is on the LambdaPlayground login page")
 public void userIsOnLoginPage() {
     System.out.println("Hello");
+    RegisterUser();
     // Hooks.driver.get("https://www.lambdatest.com/playground");
     // loginPage = new loginPage(); // ✅ initialize here after driver is ready
 }
